96b313f208c139bedd3cbeb4dbf95a37b23d95a9
tigejw <tigejw@gmail.com>
2026-09-18 14:08:17 +0200

    refactor search results to tailwind

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,17 +1,23 @@
 import SearchResults from "../components/searchResults";
 
-export default function SearchResultsPage({
-  results,
-  loading,
-  error,
-}) {
-  console.log(error, "<<<")
+export default function SearchResultsPage({ results, loading, error }) {
   return (
-    <div>
+    <div className="px-6 sm:px-12">
       <main>
-        <h2>Search Results</h2>
-        {error && <p className="error">{error}, try again later?</p>}
-        {loading ? <p>Loading...</p> : <SearchResults results={results} error={error} />}
+        <div className="flex flex-col items-start gap-4 py-6">
+          <h2 className="text-2xl font-bold leading-none">Search Results</h2>
+        </div>
+
+        {error && (
+          <p role="alert" className="text-red-600">
+            {error}, try again later?
+          </p>
+        )}
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <SearchResults results={results} error={error} />
+        )}
       </main>
     </div>
   );
